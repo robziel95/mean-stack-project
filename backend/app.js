529220ef422b11b0fe0a5231474229cd3a757527
Robert Zielinski <zielinskirob95@gmail.com>
2018-10-26 16:43:20 +0200

    created users + user token auth

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
-const postsRoutes = require("./routes/posts")
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 const app = express();
 // EnyhIPVvwPUkeqeE
@@ -32,5 +33,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, MatProgressSpinnerModule, MatPaginatorModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -10,6 +10,8 @@ import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 @NgModule({
@@ -18,12 +20,15 @@ import { AppRoutingModule } from './app-routing.module';
       PostCreateComponent,
       HeaderComponent,
       PostListComponent,
+      LoginComponent,
+      SignupComponent
 
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       ReactiveFormsModule,
+      FormsModule,
       BrowserAnimationsModule,
       MatInputModule,
       MatCardModule,
